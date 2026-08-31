90f454ad4fb6319af26a74e95bfef55589b73448
Shivako <codeako@outlook.com>
2026-08-31 17:48:18 -0500

    feat: connect API to PostgreSQL

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,42 +10,57 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-let productos = [
-    {
-        id: 1,
-        nombre: 'Botella de cobre',
-        precio: 25
-    },
-    {
-        id: 2,
-        nombre: 'Estatua decorativa',
-        precio: 40
-    },
-    {
-        id: 3,
-        nombre: 'Rastreador de hábitos',
-        precio: 15
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+pool.query('SELECT * FROM productos', (error, resultado) => {
+    if (error) {
+        console.error('Error consultando productos:', error);
+    } else {
+        console.log('Productos:', resultado.rows);
     }
-];
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hola desde mi servidor 🚀');
 });
 
-app.get('/api/productos', (req, res) => {
-    res.json(productos);
+app.get('/api/productos', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM productos');
+
+        res.json(resultado.rows);
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({
+            error: 'Error al obtener productos'
+        });
+    }
 });
 
-app.post('/api/productos', (req, res) => {
-    const nuevoProducto = {
-        id: productos.length + 1,
-        nombre: req.body.nombre,
-        precio: req.body.precio
-    };
+app.post('/api/productos', async (req, res) => {
+    try {
+        const { nombre, precio } = req.body;
 
-    productos.push(nuevoProducto);
+        const resultado = await pool.query(
+            'INSERT INTO productos (nombre, precio) VALUES ($1, $2) RETURNING *',
+            [nombre, precio]
+        );
 
-    res.status(201).json(nuevoProducto);
+        res.status(201).json(resultado.rows[0]);
+    } catch (error) {
+        console.error('Error al crear producto:', error);
+
+        res.status(500).json({
+            error: 'Error al crear producto'
+        });
+    }
 });
 
 app.listen(PORT, () => {
